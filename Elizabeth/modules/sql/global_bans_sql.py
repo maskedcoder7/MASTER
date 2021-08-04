8776db0fed1c1b15e6985644ea8e96742d1497af
maskedcoder <85559138+maskedcoder7@users.noreply.github.com>
2021-08-04 16:18:20 +0530

    Update global_bans_sql.py

--- Elizabeth/modules/sql/global_bans_sql.py
+++ Elizabeth/modules/sql/global_bans_sql.py
@@ -1,8 +1,7 @@
 import threading
 
-from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
-
-from Elizabeth.modules.sql import BASE, SESSION
+from MashaRoBot.modules.sql import BASE, SESSION
+from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 
 
 class GloballyBannedUsers(BASE):
@@ -20,10 +19,7 @@ class GloballyBannedUsers(BASE):
         return "<GBanned User {} ({})>".format(self.name, self.user_id)
 
     def to_dict(self):
-        return {
-            "user_id": self.user_id,
-            "name": self.name,
-            "reason": self.reason}
+        return {"user_id": self.user_id, "name": self.name, "reason": self.reason}
 
 
 class GbanSettings(BASE):
@@ -140,8 +136,7 @@ def num_gbanned_users():
 def __load_gbanned_userid_list():
     global GBANNED_LIST
     try:
-        GBANNED_LIST = {x.user_id for x in SESSION.query(
-            GloballyBannedUsers).all()}
+        GBANNED_LIST = {x.user_id for x in SESSION.query(GloballyBannedUsers).all()}
     finally:
         SESSION.close()
 
@@ -149,8 +144,9 @@ def __load_gbanned_userid_list():
 def __load_gban_stat_list():
     global GBANSTAT_LIST
     try:
-        GBANSTAT_LIST = {x.chat_id for x in SESSION.query(
-            GbanSettings).all() if not x.setting}
+        GBANSTAT_LIST = {
+            x.chat_id for x in SESSION.query(GbanSettings).all() if not x.setting
+        }
     finally:
         SESSION.close()
 
